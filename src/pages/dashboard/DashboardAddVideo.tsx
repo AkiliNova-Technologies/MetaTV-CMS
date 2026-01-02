38956552e13268ba92a7db93d0d461338689ba10
albert2Moon <albertwatbin@gmail.com>
2026-01-03 02:55:19 +0300

    .

--- src/pages/dashboard/DashboardAddVideo.tsx
+++ src/pages/dashboard/DashboardAddVideo.tsx
@@ -478,7 +478,7 @@ export default function DashboardAddVideo() {
 
   return (
     <div className="min-h-screen ">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         {/* Header */}
         <div className="mb-8">
           <Button
